74b16df99af7a34c5ce5f8dee60da1866ccec991
dorismeza <dorismeza56@gmail.com>
2022-02-24 18:55:34 -0600

    Api Mandados

---
+++ AppMovil2Proyecto/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const morgan = require('morgan');
+const app= express();
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.set('json space',2);
+app.listen(7000, ()=>{
+  console.log(" Servidor Iniciado en el puerto 7000 ");
+});
