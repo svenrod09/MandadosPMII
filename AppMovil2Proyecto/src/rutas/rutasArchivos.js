04603fce6c6e0580d84ac7daec6c21bd4a3fb6b5
dorismeza <dorismeza56@gmail.com>
2022-03-01 10:05:38 -0600

    Servidor de Archivos

---
+++ AppMovil2Proyecto/src/rutas/rutasArchivos.js
@@ -0,0 +1,19 @@
+const { Router } = require('express');
+const controladorArchivos= require('../controladores/controladorArchivos');
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.join(__dirname, '../public/img'));
+    },
+    filename: function(req,file,cb){
+        const unico = Date.now() + '-' + Math.round(Math.random()* 1E9);
+        cb(null, file.fieldname + '-' + unico + file.minetype.replace("/","."));
+    }
+});
+const upload = multer({
+    storage: storage,
+});
+const router = Router();
+router.post('/img', upload.single('img'), controladorArchivos.Recibir);
+module.exports = router;
