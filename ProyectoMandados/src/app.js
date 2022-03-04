31ef60984cefa40962b6ad65adafc8f696230219
Tania Villanueva <trodvi112@gmail.com>
2022-03-04 16:11:00 -0600

    Rutas Archivos

--- ProyectoMandados/src/app.js
+++ ProyectoMandados/src/app.js
@@ -13,6 +13,7 @@ app.set('json spaces', 2);
 //RUTAS
 app.use('/tienda/img', express.static(path.join(__dirname, 'public/img')));
 app.use('/producto/imgP',express.static(path.join(__dirname,'public/imgP')));
+app.use('/usuario/imgU',express.static(path.join(__dirname,'public/imgU')));
 
 app.use('/api/autenticacion/', require('./rutas/rutaAutenticacion'))
 app.use('/api/usuarios/', require('./rutas/routeUsuario'));
@@ -22,6 +23,7 @@ app.use('/api/archivos/', require('./rutas/rutasArchivos'));
 app.use('/api/detalle/', require('./rutas/rutasDetalle'));
 app.use('/api/productos/', require('./rutas/rutasProductos'));
 app.use('/api/archivosP/', require('./rutas/rutasArchivosP'));
+app.use('/api/archivosU/', require('./rutas/rutasArcuivosU'));
 
 app.listen(5000, ()=>{
     console.log('Servidor iniciado en el puerto 5000');
---
+++ ProyectoMandados/src/rutas/rutasArcuivosU.js
@@ -0,0 +1,20 @@
+const { Router } =require('express');
+const controladorArchivos = require('../controladores/controladorArchivosU');
+const multer = require('multer');
+const path = require('path');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../public/imgU'));
+    },
+    filename: function(req, file, cb){
+        const unico = Date.now() + '-' + Math.round(Math.random()* 1E9);
+        cb(null, file.fieldname + '-' + unico + file.mimetype.replace("/","."));
+    }
+});
+const upload = multer({
+    storage: storage,
+});
+const router = Router();
+
+router.post('/imgU', upload.single('imgU'), controladorArchivos.Recibir);
+module.exports= router;
