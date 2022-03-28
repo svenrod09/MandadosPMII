7afc69d1c3cb1755d9f2904ff85b285af5f40a85
dorismeza <dorismeza56@gmail.com>
2022-03-28 14:25:36 -0600

    .

--- ProyectoMandados/src/configuracion/db.js
+++ ProyectoMandados/src/configuracion/db.js
@@ -1,8 +1,8 @@
 const sequelize = require('sequelize');
 const bd = new sequelize(
     'mandados',
-    'doris',
-    '123456',
+    'root',
+    '1234',
     {
         host:'localhost',
         dialect: 'mysql',
